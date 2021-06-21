f156ad4e74805b7e0de2b886f9b9e708f6a9e865
KaranPatel1005 <kgkaran97@gmail.com>
2021-06-21 23:38:52 +0530

    changes in files

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React from 'react';
 
 function App() {
     return (
-       <div>
-           <form type="submit">
-               <input type="textarea"></input>
-               <span>Wrtite something!</span>
-               <button type="submit"></button>
-           </form>
-       </div>
+        <div>
+            <h1>TodoMatic</h1>
+            <form>
+                <h2 className="">
+                    <label htmlFor="" className="">What need to be done?</label>
+                </h2>
+
+                <input
+                    type="text"
+                    id="todo-input"
+                    className=""
+                    name="text"
+                    autoComplete="off" />
+                <span>Wrtite something!</span>
+                <button
+                    type="submit"
+                    className="">Add</button>
+            </form>
+            <div className="">
+                <button
+                    type="button" className="" aria-pressed="true">
+                    <span className>Show </span>
+                    <span>all</span>
+                    <span className> tasks</span>
+                </button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+
+            <input type="radio"></input><span>Radio</span>
+        </div>
 
     );
 }
